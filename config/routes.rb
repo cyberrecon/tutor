7b32c5ea8f49a0d2353ce16092f93e20b0528dd7
sweeper@chrisd.nl <sweeper@chrisd.nl>
2013-07-20 02:52:39 +0000

    added route for /

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Blog::Application.routes.draw do
 
   resources :articles
 
+  root :to => redirect('/products')
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
